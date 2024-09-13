a24c0dcb0df2a2e6e203464c31f9f8a13562898e
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2024-09-13 19:50:50 +0300

    feat: check src directory for codestyle

--- tests/test_codestyle.py
+++ tests/test_codestyle.py
@@ -1,8 +1,24 @@
 import pycodestyle
+import os
+
+
+def check_dir(dir_name):
+    wrong_codestyle_files = []
+
+    for root, _, files in os.walk(dir_name):
+        path = root.split(os.sep)
+        for file in files:
+            file_path = "/".join(path) + "/" + file
+
+            if file_path.endswith(".py"):
+                checker = pycodestyle.Checker(file_path, show_source=False, quiet=True)
+                if checker.check_all() != 0:
+                    wrong_codestyle_files.append(file_path)
+
+    return wrong_codestyle_files
 
 
 def test_codestyle():
-    fchecker = pycodestyle.Checker("src/main.py", show_source=False, quiet=True)
-    codestyle_errors = fchecker.check_all()
+    wrong_codestyle_files = check_dir("src")
 
-    assert codestyle_errors == 0
+    assert len(wrong_codestyle_files) == 0, "; ".join(wrong_codestyle_files)
