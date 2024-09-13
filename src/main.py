a5ef40054e2371b08b0a13775e0055d3fda8672d
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2024-09-13 15:32:56 +0300

    feat: code testing

---
+++ src/main.py
@@ -0,0 +1,5 @@
+def add_correct(a, b): 
+    return a + b
+
+def add_wrong(a, b): 
+    return a + b + 1 
---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.main import add_correct, add_wrong
+
+def test_add_correct(): 
+    assert add_correct(1, 2) == 3
+    assert add_correct(2, 1) == 3
+    assert add_correct(-1, -2) == -3
+    assert add_correct(0, 0) == 0
+
+def test_add_wrong(): 
+    assert add_wrong(1, 2) == 3
+    assert add_wrong(2, 1) == 3
+    assert add_wrong(-1, -2) == -3
+    assert add_wrong(0, 0) == 0
+
